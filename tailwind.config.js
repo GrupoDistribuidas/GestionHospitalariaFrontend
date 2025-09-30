bd07f691996d102ee725dd3696fe2fa0c40a6d4b
xxxMichael <mayckolchavez27@gmail.com>
2025-09-30 14:47:17 -0500

    Actualizar títulos y mejorar la configuración de Tailwind y Vite para mejorar rendimiento

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+  // Purge CSS no utilizado
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{js,jsx,ts,tsx}']
+  }
+}
